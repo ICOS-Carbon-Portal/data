34728283f0125de838ca9575d34ef39f4b0e28c0
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-04-04 13:56:24 +0200

    Finalized MassUpload rewrite

--- src/main/scala/se/lu/nateko/cp/data/MassUpload.scala
+++ src/main/scala/se/lu/nateko/cp/data/MassUpload.scala
@@ -2,12 +2,19 @@ package se.lu.nateko.cp.data
 
 import java.io.File
 import java.net.URI
+
 import scala.collection.immutable.Seq
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import scala.util.Failure
 import scala.util.Success
+import scala.util.Try
+
+import akka.Done
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.HostConnectionPool
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.sprayJsonMarshaller
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.ContentTypes
@@ -15,29 +22,26 @@ import akka.http.scaladsl.model.FormData
 import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.model.HttpMethods
 import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.RequestEntity
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.model.headers.Cookie
 import akka.http.scaladsl.model.headers.HttpCookiePair
 import akka.http.scaladsl.model.headers.`Set-Cookie`
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.FileIO
+import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
 import se.lu.nateko.cp.data.streams.DigestFlow
 import se.lu.nateko.cp.meta.core.CommonJsonSupport
 import se.lu.nateko.cp.meta.core.crypto.Sha256Sum
-import akka.NotUsed
-import akka.stream.scaladsl.Flow
-import akka.http.scaladsl.model.HttpResponse
-import akka.http.scaladsl.Http.HostConnectionPool
-import scala.util.Try
-import akka.http.scaladsl.model.StatusCodes
-import scala.util.Failure
 
 object MassUpload extends CommonJsonSupport{
 
+	val parall = 8
 	val Username = "test@upload"
 	val Producer = "http://meta.icos-cp.eu/ontologies/cpmeta/instances/WDCGG"
 	val ObjSpec = "http://meta.icos-cp.eu/ontologies/cpmeta/instances/wdcggDataObject"
@@ -62,69 +66,43 @@ object MassUpload extends CommonJsonSupport{
 	var cookie = HttpCookiePair("dummy", "dummy")
 
 	type HttpFlow[T] = Flow[(HttpRequest, T), (Try[HttpResponse], T), HostConnectionPool]
-	type ConnFlow = Flow[HttpRequest, HttpResponse, Future[Http.OutgoingConnection]]
+	type StepResult[T] = (T, Option[String])
 
 	def metaHttp[T]: HttpFlow[T] = http.cachedHostConnectionPoolHttps("meta.icos-cp.eu")
 	def dataHttp[T]: HttpFlow[T] = http.cachedHostConnectionPoolHttps("data.icos-cp.eu")
-	def dataConn: ConnFlow = http.outgoingConnectionHttps("data.icos-cp.eu")
 
 	//def metaHttp[T]: HttpFlow[T] = http.cachedHostConnectionPool("127.0.0.1", 9094)
 	//def dataHttp[T]: HttpFlow[T] = http.cachedHostConnectionPool("127.0.0.1", 9010)
-	//def dataConn: ConnFlow = http.outgoingConnection("127.0.0.1", 9010)
 	
-	val fileSource: Source[File, Future[Int]] = {
+	val fileSource: Source[File, NotUsed] = {
 		def getFiles(folder: File): Seq[File] = {
 			val children = Seq(folder.listFiles :_*)
 			children.filter(_.isFile) ++ children.filter(_.isDirectory).flatMap(getFiles)
 		}
-		val files = getFiles(new File(RootFolder))
-		Source(files).mapMaterializedValue(_ => Future.successful(files.size))
+		Source(getFiles(new File(RootFolder)))
 	}
 
-	val metaSubmittingFlow: Flow[File, FileInfo, NotUsed] = Flow[File]
-		.mapAsyncUnordered(8){file =>
-			val hashFut = FileIO.fromFile(file)
-				.viaMat(DigestFlow.sha256)(Keep.right)
-				.to(Sink.ignore).run()
-			hashFut.flatMap(hash => metaSubmission(FileInfo(file, hash)))
-		}
+	val fileInfoSource: Source[FileInfo, NotUsed] = fileSource.mapAsyncUnordered(parall){file =>
+		FileIO.fromFile(file)
+			.viaMat(DigestFlow.sha256)(Keep.right)
+			.to(Sink.ignore).run()
+			.map(hash => FileInfo(file, hash))
+	}
+
+	val metaSubmittingFlow: Flow[FileInfo, StepResult[FileInfo], NotUsed] = Flow[FileInfo]
+		.mapAsyncUnordered(parall){metaSubmission}
 		.via(metaHttp)
-		.collect{
-			case (Success(resp), fileInfo) if resp.status == StatusCodes.OK => fileInfo
-		}
+		.via(uploadResultControl)
 
-	def count[T]: Sink[T, Future[Int]] = Sink.fold[Int, T](0)((c, _) => c + 1)
-
-	def fileUploadingSink[T](uploader: T => (HttpRequest, T)): Sink[T, Future[Int]] = Flow[T]
-		.map(uploader)
-		.log("file upl.", _._2)
+	val fileUploadingFlow: Flow[FileInfo, StepResult[FileInfo], NotUsed] = Flow[FileInfo]
+		.map(fileSubmission)
 		.via(dataHttp)
-		.log("Upload result", {
-			case (Success(resp), t)  => s"${resp.status}: $t"
-			case (Failure(err), t)  => s"{err.getMessage}: $t"
-		})
-		.collect{
-			case (Success(resp), t) if resp.status == StatusCodes.OK => t
-		}
-		.toMat(count)(Keep.right)
+		.via(uploadResultControl)
 
-	val fileDumpingSink: Sink[File, Future[Int]] = fileUploadingSink(fileDumping)
-
-	def dumpFiles: Future[(Int, Int)] = fileSource.toMat(fileDumpingSink)(_ zip _).run()
-
-	def dumpFilesConn = fileSource
-		.log("File")
-		.map(fileDump).viaMat(dataConn)(Keep.right)
-		.mapAsync(8)(resp => resp.entity.toStrict(1 second))
-		.map(_.data.utf8String)
-		.log("Response")
-		.toMat(count)(Keep.both).run()
-
-	def getMany = Source(1 to 1000).map(_ => HttpRequest(uri = "/portal/")).via(dataConn)
-		.map(x => {println(x.status); x})
-		.mapAsync(1)(resp => resp.entity.toStrict(1 second))
-		.map(_.data.utf8String)
-		.runWith(count)
+	val fileDumpingFlow: Flow[File, StepResult[File], NotUsed] = Flow[File]
+		.map(fileDumping)
+		.via(dataHttp)
+		.via(uploadResultControl)
 
 	def initAuthCookie(password: String): Future[HttpCookiePair] =
 		Marshal(FormData(Uri.Query("mail" -> Username, "password" -> password)))
@@ -146,7 +124,35 @@ object MassUpload extends CommonJsonSupport{
 				case Success(pair) => cookie = pair
 			}
 
-	def metaSubmission(fileInfo: FileInfo): Future[(HttpRequest, FileInfo)] = {
+	def dumpFiles = fileSource.via(fileDumpingFlow).runWith(resultLogger(f => s"Dumping ${f.getName}"))
+
+	def submitFiles = fileInfoSource
+		.via(metaSubmittingFlow)
+		.alsoTo(resultLogger(f => s"Meta upload ${f.file.getName}"))
+		.collect{case (fi, None) => fi}
+		.via(fileUploadingFlow)
+		.runWith(resultLogger(f => s"File upload ${f.file.getName}"))
+
+	private def uploadResultControl[T]: Flow[(Try[HttpResponse], T), StepResult[T], NotUsed] = Flow[(Try[HttpResponse], T)]
+		.mapAsyncUnordered(parall){
+			case (Success(resp), payload) if resp.status == StatusCodes.OK =>
+				Future.successful((payload, None))
+			case (Success(resp), payload) =>
+				resp.entity.toStrict(1 second).map{entity =>
+					val responseText = entity.data.utf8String
+					val msg = resp.status.defaultMessage + ": " + responseText
+					(payload, Some(msg))
+				}
+			case (Failure(err), payload) =>
+				Future.successful((payload, Some(err.getMessage)))
+		}
+
+	private def resultLogger[T](toStr: T => String): Sink[StepResult[T], Future[Done]] = Sink.foreach{
+		case (t, None) => system.log.info(toStr(t))
+		case (t, Some(msg)) => system.log.error(toStr(t) + ": " + msg)
+	}
+
+	private def metaSubmission(fileInfo: FileInfo): Future[(HttpRequest, FileInfo)] = {
 		val meta = UploadMetadataDto(
 			hashSum = fileInfo.hash,
 			submitterId = "CP",
@@ -167,7 +173,7 @@ object MassUpload extends CommonJsonSupport{
 			)}
 	}
 
-	def fileUpload(file: File, pathSuffix: String): HttpRequest = {
+	private def fileUpload(file: File, pathSuffix: String): HttpRequest = {
 		val entity = HttpEntity(ContentTypes.`application/octet-stream`, FileIO.fromFile(file))
 		HttpRequest(
 			method = HttpMethods.PUT,
@@ -177,11 +183,9 @@ object MassUpload extends CommonJsonSupport{
 		)
 	}
 
-	def fileDump(file: File): HttpRequest = fileUpload(file, "dump")
-
-	def fileSubmission(fileInfo: FileInfo): (HttpRequest, FileInfo) =
+	private def fileSubmission(fileInfo: FileInfo): (HttpRequest, FileInfo) =
 		(fileUpload(fileInfo.file, fileInfo.hash.id), fileInfo)
 
-	def fileDumping(file: File): (HttpRequest, File) = (fileDump(file), file)
+	private def fileDumping(file: File): (HttpRequest, File) = (fileUpload(file, "dump"), file)
 
 }
