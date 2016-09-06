a0f51c445c4484f7bc9dbb2e5291f7583e1545a9
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-09-06 12:08:05 +0200

    Made FootprintsRegistry much more robust

--- src/main/js/stilt/main/models/FootprintsRegistry.js
+++ src/main/js/stilt/main/models/FootprintsRegistry.js
@@ -1,19 +1,21 @@
 
 export default class FootprintsRegistry{
 	constructor(filenames){
-		filenames.sort();
-		const getDate = i => datetimeFromFile(filenames[i]);
+		let dates = filenames.map(datetimeFromFile);
+		dates.sort((d1, d2) => d1 - d2);
 
-		const n = filenames.length
-		const minD = getDate(0);
-		const lastD = getDate(n - 1);
-		const step = (lastD - minD) / (n - 1);
+		let counts = {};
+		for(let i = 1; i < dates.length; i++){
+			let step = dates[i] - dates[i - 1];
+			counts[step] = (counts[step] || 0) + 1;
+		}
 
+		let steps = Object.keys(counts);
+		steps.sort((s1, s2) => counts[s2] - counts[s1]);
 
-		this._filenames = filenames;
-		this._minD = minD;
-		this._maxD = lastD + step;
-		this._step = step;
+		this._dates = dates;
+		this._step = steps[0];
+		this._filenameSuffix = filenames[0].substr(17);
 	}
 
 	get dateRange(){
@@ -21,11 +23,32 @@ export default class FootprintsRegistry{
 	}
 
 	getRelevantFilename(date){
-		const millis = date.valueOf();
-		const index = Math.floor((millis - this._minD) / this._step);
-		const n = this._filenames.length - 1;
-		const safeIndex = index < 0 ? 0 : index > n ? n : index;
-		return this._filenames[safeIndex];
+		const dates = this._dates;
+		const step = this._step;
+		const d = date.valueOf();
+
+		function improve(left, right){
+			if(right == left) return left;
+			const dmin = dates[left];
+			const dmax = dates[right];
+			if(right - left == 1) return (d - dmin > dmax - d) ? right : left;
+			if(d <= dmin + step / 2) return left;
+			if(d > dmax - step / 2) return right;
+
+			const slope = (right - left) / (dmax - dmin);
+			let guess = left + Math.round(slope * (d - dmin));
+			if(guess == left || guess == right) guess = Math.round((left + right) / 2);
+
+			return dates[guess] > d ? improve(left, guess) : improve(guess, right);
+		}
+
+		const index = improve(0, dates.length - 1);
+		return this.constructFilename(dates[index]);
+	}
+
+	constructFilename(date){
+		const d = new Date(date);
+		return `foot${d.getUTCFullYear()}x${pad2(d.getUTCMonth() + 1)}x${pad2(d.getUTCDate())}x${pad2(d.getUTCHours())}${this._filenameSuffix}`;
 	}
 }
 
@@ -35,6 +58,10 @@ const fileRegex = /^foot(\d{4})x(\d\d)x(\d\d)x(\d\d)/;
 export function datetimeFromFile(filename){
 	const match = fileRegex.exec(filename);
 	const [year, month, day, hour] = [1, 2, 3, 4].map(i => parseInt(match[i]));
-	return Date.UTC(year, month, day, hour);
+	return Date.UTC(year, month - 1, day, hour, 0, 0); //months are zero-based!
+}
+
+function pad2(num){
+	return ('0' + num).slice(-2);
 }
 
