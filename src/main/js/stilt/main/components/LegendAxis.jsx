6d03d3a2cb8600f31033f6b45e1d77740417a456
Roger Groth <roger.groth@gis.lu.se>
2016-09-09 11:40:24 +0200

    Fixed code indentation

--- src/main/js/stilt/main/components/LegendAxis.jsx
+++ src/main/js/stilt/main/components/LegendAxis.jsx
@@ -14,17 +14,17 @@ export default class LegendAxis extends Component {
 			<svg ref="axis" className="axis" width={props.width + props.margin * 2} height={props.height + textMargin} style={{display: 'block'}}>{
 				props.suggestedTickLocations
 					? props.suggestedTickLocations.map((tick, idx) => {
-					const tickVal = props.valueMaker(tick) == 1
-						? 1
-						: props.valueMaker(tick).toExponential(0)
+						const tickVal = props.valueMaker(tick) == 1
+							? 1
+							: props.valueMaker(tick).toExponential(0)
 
-					return (
-						<g key={'g' + idx}>
-							<line x1={tick + props.margin} y1={0} x2={tick + props.margin} y2={10} stroke="black" strokeWidth="2" />
-							<text x={tick + props.margin} y={props.margin + textMargin} textAnchor="middle">{tickVal}</text>
-						</g>
-					);
-				})
+						return (
+							<g key={'g' + idx}>
+								<line x1={tick + props.margin} y1={0} x2={tick + props.margin} y2={10} stroke="black" strokeWidth="2" />
+								<text x={tick + props.margin} y={props.margin + textMargin} textAnchor="middle">{tickVal}</text>
+							</g>
+						);
+					})
 					: null
 			}</svg>
 		);
