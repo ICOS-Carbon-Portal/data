49e04b43273fed0f49745048cab6d14f13c74e39
Roger Groth <roger.groth@gis.lu.se>
2017-04-12 10:10:07 +0200

    The Params class now handle older browser (like IE) that does not have support for URLSearchParams

--- src/main/js/netcdf-light/main/models/Params.js
+++ src/main/js/netcdf-light/main/models/Params.js
@@ -1,14 +1,48 @@
-import 'babel-polyfill';
-
 export default class Params {
 	constructor(search, required){
-		this._params = new URLSearchParams(search);
-		this.isValidParams = required.reduce((acc, curr) => acc * this._params.has(curr), true);
+		this._params = this.parseSearch(search);
+		this.isValidParams = this.validateSearch(required);
+	}
+
+	parseSearch(search) {
+		return window.URLSearchParams
+			? new URLSearchParams(search)
+			: search;
+	}
+
+	validateSearch(required){
+		if (window.URLSearchParams) {
+			return required.reduce((acc, curr) => acc * this._params.has(curr), true);
+		} else {
+			return required.reduce((acc, curr) => {
+				const regex = new RegExp('[\\?&]' + curr + '=([^&#]*)');
+				const results = regex.exec(this._params);
+				return acc * (results !== null);
+			}, true);
+		}
 	}
 
 	get(param){
-		return this._params.has(param)
-			? this._params.get(param) && this._params.get(param) !== 'null' ? this._params.get(param) : undefined
-			: null;
+		// A non existing search param -> return null
+		// An existing search param that has value null or nothing -> return undefined
+		// An existing search param that has a value other than null -> return the value
+		if (window.URLSearchParams) {
+			return this._params.has(param)
+				? this._params.get(param) && this._params.get(param) !== 'null' ? this._params.get(param) : undefined
+				: null;
+		} else {
+			return this.getUrlParameter(param);
+		}
+	}
+
+	getUrlParameter(param) {
+		param = param.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+		const regex = new RegExp('[\\?&]' + param + '=([^&#]*)');
+		const results = regex.exec(this._params);
+		return results === null
+			? null
+			: results[1] === '' || results[1] === 'null'
+				? undefined
+				: decodeURIComponent(results[1].replace(/\+/g, ' '));
 	}
 }
