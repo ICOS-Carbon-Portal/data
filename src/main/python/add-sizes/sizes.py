12aa1e35c14ef6439d1ebaa92ca77df8e089f256
Roger Groth <roger.groth@gis.lu.se>
2017-10-26 10:27:15 +0200

    Production ready, python 2

--- src/main/python/add-sizes/sizes.py
+++ src/main/python/add-sizes/sizes.py
@@ -2,11 +2,15 @@ import os
 from datetime import datetime
 from subprocess import check_output
 
-BASE_FOLDER = "/disk/data/dataAppStorage"
-OUTPUT_FOLDER = "/disk/volumes/rdflogdb"
-PG_FOLDER = "/var/lib/postgresql/data"
-CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+IS_PRODUCTION = True
+IS_SIMULATION = True
 
+RDF_CONTAINER = "rdflogdb-v9.6" if IS_PRODUCTION else "rdflogdb"
+OUTPUT_FOLDER = "/disk/data/volumes/rdflogdb-9.6/add-sizes" if IS_PRODUCTION else "/disk/volumes/rdflogdb/pgdata/add-sizes"
+DATA_FOLDER = "/disk/data/dataAppStorage"
+PG_FOLDER = "/var/lib/postgresql/data/pgdata/add-sizes"
+
+# Only folders mentioned here will be processed
 FOLDER_2_TABLE = {
 	'asciiWdcggTimeSer': 'wdcgg',
 	'asciiAtcTimeSer': 'atmcsv',
@@ -35,8 +39,8 @@ def get_folders(dirname):
 
 def process_folder(folder):
 	file_data = []
-	for basename in os.listdir(os.path.join(BASE_FOLDER, folder)):
-		filename = os.path.join(BASE_FOLDER, folder, basename)
+	for basename in os.listdir(os.path.join(DATA_FOLDER, folder)):
+		filename = os.path.join(DATA_FOLDER, folder, basename)
 		size = os.path.getsize(filename)
 		extension = os.path.splitext(filename)[1][1:]
 
@@ -51,7 +55,7 @@ def copy_to_table(folder, sizes):
 		table_name = FOLDER_2_TABLE[folder]
 		output_file = os.path.join(OUTPUT_FOLDER, table_name + ".csv")
 
-		with open(output_file, "w") as sql_file:
+		with open(output_file, "w") as csv_file:
 			for size in sizes:
 				table_row = [
 					datetime.now().isoformat(),
@@ -61,26 +65,32 @@ def copy_to_table(folder, sizes):
 					'http://meta.icos-cp.eu/ontologies/cpmeta/hasSizeInBytes', str(size[1]),
 					'http://www.w3.org/2001/XMLSchema#long'
 				]
-				sql_file.write(",".join(table_row) + "\n")
+				csv_file.write(",".join(table_row) + "\n")
 
-		table_exists_query = check_output("docker exec -t rdflogdb psql -U postgres -t -c \"SELECT 1 FROM information_schema.tables WHERE table_name = '" + table_name + "';\"", shell=True)
+		table_exists_query = check_output(
+			"docker exec -t " + RDF_CONTAINER + " psql -U postgres -t -c \"SELECT 1 FROM information_schema.tables WHERE table_name = '" + table_name + "';\"",
+			shell=True)
 
 		if table_exists_query != "":
-			result = check_output("docker exec -t rdflogdb psql -U postgres -c \"COPY " + table_name
-								  + "(tstamp, \\\"ASSERTION\\\", \\\"TYPE\\\", \\\"SUBJECT\\\", \\\"PREDICATE\\\", \\\"OBJECT\\\", \\\"LITATTR\\\") "
-								  + "FROM '/var/lib/postgresql/data/" + table_name + ".csv' DELIMITER ',' CSV;\"", shell=True)
-			expected_num = len(sizes)
-			actual_num = int(result.replace("COPY ", ""))
-			print "Expected number of inserts for table " + table_name + ":", str(expected_num), "Actual result:", str(actual_num)
-			if expected_num != actual_num:
-				print "ERROR - Expected number of inserted rows does not match actual inserted rows!"
+			if IS_SIMULATION:
+				print "Simulating adding data to table " + table_name + " (" + str(len(sizes)) + " records)"
+			else:
+				result = check_output("docker exec -t " + RDF_CONTAINER + " psql -U postgres -c \"COPY " + table_name
+					+ "(tstamp, \\\"ASSERTION\\\", \\\"TYPE\\\", \\\"SUBJECT\\\", \\\"PREDICATE\\\", \\\"OBJECT\\\", \\\"LITATTR\\\") "
+					+ "FROM '" + os.path.join(PG_FOLDER, table_name + ".csv") + "' DELIMITER ',' CSV;\"", shell=True)
+				expected_num = len(sizes)
+				actual_num = int(result.replace("COPY ", ""))
+				print "Expected number of inserts for table " + table_name + ":", str(expected_num), "Actual result:", str(actual_num)
+				if expected_num != actual_num:
+					print "ERROR - Expected number of inserted rows does not match actual inserted rows!"
 		else:
 			print "Table " + table_name + " does not exist. Skipping import of " + table_name + ".csv"
 
-		os.remove(output_file)
 
+if not os.path.exists(OUTPUT_FOLDER):
+	os.makedirs(OUTPUT_FOLDER)
 
-folders = get_folders(BASE_FOLDER)
+folders = get_folders(DATA_FOLDER)
 
 for folder in folders:
 	copy_to_table(folder, process_folder(folder))
