375ba3f3534b805b35fdb272b5df77238df78450
Jonathan Thiry <github@jonathanthiry.com>
2018-03-27 12:35:17 +0200

    [portal] Use envri to config restheart endpoint

--- src/main/js/common/main/config.js
+++ src/main/js/common/main/config.js
@@ -2,8 +2,8 @@
 //var endpoint = 'http://127.0.0.1:9094/sparql';
 var endpoint = 'https://meta.icos-cp.eu/sparql';
 
-const host = location.host.replace('data', 'meta');
 const envri = location.host.indexOf('fieldsites.se') >= 0 ? 'SITES' : 'ICOS';
+const host = envri == 'SITES' ? 'meta.fieldsites.se' : 'meta.icos-cp.eu';
 
 export default {
 	sparqlEndpoint: endpoint,
