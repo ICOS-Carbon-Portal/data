0604200fb7b9c2c137721111fc15960528f5596a
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2019-02-19 19:25:59 +0100

    [backend] More robust FileSavingUploadTask

--- src/main/scala/se/lu/nateko/cp/data/services/upload/FileSavingUploadTask.scala
+++ src/main/scala/se/lu/nateko/cp/data/services/upload/FileSavingUploadTask.scala
@@ -12,6 +12,7 @@ import akka.stream.IOResult
 import akka.stream.scaladsl.FileIO
 import akka.stream.scaladsl.Sink
 import akka.util.ByteString
+import se.lu.nateko.cp.data.api.CpDataException
 
 class FileSavingUploadTask(file: File)(implicit ctxt: ExecutionContext) extends UploadTask{
 
@@ -29,10 +30,21 @@ class FileSavingUploadTask(file: File)(implicit ctxt: ExecutionContext) extends 
 		ownResult match {
 			case FileExists => Future.successful(FileExists)
 			case _ =>
-				val relevantOtherErrors = otherTaskResults.collect{
-					case failure: HashsumCheckFailure => failure
-					case failure: UnexpectedTaskFailure => failure
-				}
+				val relevantOtherErrors: Seq[UploadTaskFailure] = otherTaskResults.collect{
+					case failure: HashsumCheckFailure => Some(failure)
+					case failure: UnexpectedTaskFailure => Some(failure)
+					case failure: ByteCountingFailure => Some(failure)
+
+					case ByteCountingSuccess(bytes) =>
+						ownResult match{
+							case FileWriteSuccess(actualBytes) =>
+								if(bytes == actualBytes) None else {
+									val msg = s"Got $bytes bytes but written to disk only $actualBytes"
+									Some(ByteCountingFailure(new CpDataException(msg)))
+								}
+							case _ => None
+						}
+				}.flatten
 
 				UploadTask.revertOnAnyFailure(ownResult, relevantOtherErrors, () => Future{
 					if(file.exists) file.delete()
