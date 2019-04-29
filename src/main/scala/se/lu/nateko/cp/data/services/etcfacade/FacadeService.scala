ICOS-Carbon-Portal/data

commit 7c1768eaed057de4504740b0c7373cb8619bd014

[etcfacade] Changed logic for whether EC packaging and upload must happen immediately

src/main/scala/se/lu/nateko/cp/data/services/etcfacade/FacadeService.scala
@@ -133,7 +133,7 @@ class FacadeService(val config: EtcFacadeConfig, upload: UploadService)(implicit
 		if(fn.station == config.testStation) done else performUpload(file, fn, forceEc)
 
 	private def performUpload(file: Path, fn: EtcFilename, forceEc: Boolean) = (fn.toEcDaily match{
-		case Some(daily) if(isFromBeforeToday(daily)) =>
+		case Some(daily) =>
 
 			val uploadedFolder = getStationUploadedFolder(fn.station)
 			Files.createDirectories(uploadedFolder)
@@ -144,7 +144,7 @@ class FacadeService(val config: EtcFacadeConfig, upload: UploadService)(implicit
 			val filePackage = fresh ++ uploaded
 			val isFullPackage: Boolean = packageIsComplete(filePackage)
 
-			if(isFullPackage || forceEc){
+			if(isFullPackage && uploaded.isEmpty || forceEc){
 
 				val srcFiles = filePackage.map(_._1).sortBy(_.getFileName.toString)
 
@@ -158,11 +158,11 @@ class FacadeService(val config: EtcFacadeConfig, upload: UploadService)(implicit
 						}
 						performEtcUpload(file, daily, Some(hash))
 				}
-			} else done //no uploads for incomplete packages, unless forced
+			} else done //no uploads for incomplete or previously incomplete packages, unless forced
+
 		case None =>
 			performEtcUpload(file, fn, None)
-		case _ =>
-			done //no uploads for same-day EC files (more are likely coming!)
+
 	}).andThen{
 		case Failure(err) =>
 			appendError(s"Error while uploading $fn : " + err.getMessage)
@@ -284,8 +284,6 @@ object FacadeService{
 			}
 			.run()
 	}
-
-	def isFromBeforeToday(fn: EtcFilename): Boolean = LocalDate.now(ZoneOffset.UTC).compareTo(fn.date) > 0
 
 	private def packageIsComplete(fileInfos: Vector[EtcFileInfo]): Boolean =
 		fileInfos.map(_._2.slot).flatten.distinct.size == 48

.gitignore
@@ -26,6 +26,7 @@ project/target/
 .cache-main
 .cache-tests
 .metals/
+.bloop/
 .vscode/
 /bin/
 /views/bin/

commit 065107042521823cff7d5f8d9b09a3bc229c4bfa

[etcfacade] Fixed an EC upload logic regression introduced in 7c1768e

src/main/scala/se/lu/nateko/cp/data/services/etcfacade/FacadeService.scala
@@ -144,7 +144,7 @@ class FacadeService(val config: EtcFacadeConfig, upload: UploadService)(implicit
 			val filePackage = fresh ++ uploaded
 			val isFullPackage: Boolean = packageIsComplete(filePackage)
 
-			if(isFullPackage && uploaded.isEmpty || forceEc){
+			if(isFullPackage && uploaded.isEmpty || forceEc && isFromBeforeToday(daily)){
 
 				val srcFiles = filePackage.map(_._1).sortBy(_.getFileName.toString)
 
@@ -284,6 +284,8 @@ object FacadeService{
 			}
 			.run()
 	}
+
+	def isFromBeforeToday(fn: EtcFilename): Boolean = LocalDate.now(ZoneOffset.UTC).compareTo(fn.date) > 0
 
 	private def packageIsComplete(fileInfos: Vector[EtcFileInfo]): Boolean =
 		fileInfos.map(_._2.slot).flatten.distinct.size == 48