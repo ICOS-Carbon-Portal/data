2cbb6847f9860ec5f79e231e8ff6ceedcdb2be5d
Roger Groth <roger.groth@gis.lu.se>
2019-06-18 13:24:32 +0200

    Working state for netcdf

--- src/main/js/netcdf/gulpfile.babel.js
+++ src/main/js/netcdf/gulpfile.babel.js
@@ -1,9 +1,6 @@
 'use strict';
 
 import gulp from 'gulp';
-import gp_uglify from 'gulp-uglify';
-import gp_replace from 'gulp-replace';
-import buffer from 'vinyl-buffer';
 import del from 'del';
 
 import buildConf from '../common/main/buildConf.js';
@@ -11,42 +8,36 @@ import buildConf from '../common/main/buildConf.js';
 const currentPath = __dirname;
 const project = currentPath.split('/').pop();
 
+const replaceSearch = "url(node_modules/icos-cp-netcdfmap/dist/images/";
+const replacement = "url(/style/netcdf/images/";
+
 const paths = {
 	main: 'main/main.jsx',
-	jsx: 'main/**/*.jsx',
-	js: 'main/**/*.js',
+	src: 'main/**/*.js*',
 	commonjs: '../common/main/**/*.js*',
 	imagesSource: 'node_modules/icos-cp-netcdfmap/dist/**/*.png',
-	styleTargetDir: '../../resources/style/netcdf/',
-	target: '../../resources/',
+	styleTargetDir: buildConf.buildTarget + 'style/' + project + '/',
 	bundleFile: project + '.js'
 };
 
 const clean = _ => {
-	return del([paths.target + paths.bundleFile, paths.styleTargetDir], {force: true});
+	return del([buildConf.buildTarget + paths.bundleFile, paths.styleTargetDir], {force: true});
+};
+
+const compileSrc = _ => {
+	const isProduction = process.env.NODE_ENV === 'production';
+
+	return buildConf.transformToBundle(isProduction, paths, {replaceSearch, replacement});
 };
 
 const copyImages = _ => {
 	return gulp.src(paths.imagesSource).pipe(gulp.dest(paths.styleTargetDir));
 };
 
-const compileJs = _ =>  {
-	const isProduction = process.env.NODE_ENV === 'production';
 
-	let stream = buildConf.transformToBundle(isProduction, paths);
+gulp.task('build', gulp.series(clean, copyImages, compileSrc));
 
-	stream = isProduction
-		? stream
-			.pipe(buffer())
-			.pipe(gp_uglify())
-		: stream;
-
-	return stream
-		.pipe(gp_replace('url(node_modules/icos-cp-netcdfmap/dist/images/', 'url(/style/netcdf/images/'))
-		.pipe(gulp.dest(paths.target));
-};
-
-gulp.task('build', gulp.series(clean, copyImages, compileJs));
+gulp.task('buildWatch', gulp.series('build', buildConf.watch([paths.src], gulp.series('build'))));
 
 gulp.task('publish', gulp.series(buildConf.applyProdEnvironment, 'build'));
 
