16214b1806a005a94ce91a26b57fcaed4b861d4a
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2019-11-21 15:50:29 +0100

    [map-graph] Switching to ts-based icos-cp-backend 0.4.0; other projs stay on 0.3.20

--- src/main/js/map-graph/main/backend.ts
+++ src/main/js/map-graph/main/backend.ts
@@ -1,9 +1,8 @@
-import 'babel-polyfill';
-import {sparql, getBinaryTable, tableFormatForSpecies} from 'icos-cp-backend';
-import {objectSpecification} from './sparqlQueries';
+import {sparql, getBinaryTable, tableFormatForSpecies, TableFormat, BinTable} from 'icos-cp-backend';
+import {objectSpecification, Config} from './sparqlQueries';
 
 
-export function getTableFormatNrows(config, objId){
+export function getTableFormatNrows(config: Config, objId: string){
 	const query = objectSpecification(config, objId);
 
 	return sparql(query, config.sparqlEndpoint)
@@ -29,8 +28,8 @@ export function getTableFormatNrows(config, objId){
 		);
 }
 
-export function getBinTable(objId, tableFormat, nRows){
-	const axisIndices = Array.from({length: tableFormat._columnsInfo.length}, (_, i) => i);
+export function getBinTable(objId: string, tableFormat: TableFormat, nRows: number): Promise<BinTable>{
+	const axisIndices = Array.from({length: tableFormat.columns.length}, (_, i) => i);
 	const request = tableFormat.getRequest(objId, nRows, axisIndices);
 	return getBinaryTable(request, '/portal/tabular');
 }
--- src/main/js/map-graph/main/sparqlQueries.js
+++
@@ -1,9 +0,0 @@
-export function objectSpecification(config, objId){
-	return `prefix cpmeta: <${config.cpmetaOntoUri}> 
-select * where{
-	bind (<${config.cpmetaObjectUri}${objId}> as ?dobj)
-	?dobj cpmeta:hasObjectSpec ?objSpec ;
-		cpmeta:hasNumberOfRows ?nRows .
-	OPTIONAL{?dobj cpmeta:hasActualColumnNames ?colNames}
-}`;
-}
