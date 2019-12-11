61c0904ce46736408f0ea6976cdf794f217ee15e
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2019-12-11 10:41:10 +0100

    [dygraph-light] More robust SPARQL query

--- src/main/js/dygraph-light/main/sparqlQueries.ts
+++ src/main/js/dygraph-light/main/sparqlQueries.ts
@@ -11,7 +11,7 @@ export type Config = {
 export function objectSpecification(config: Config, objIds: string[]): SpecQuery{
 	const ids = objIds.map(id => `<${config.cpmetaObjectUri}${id}>`).join(' ');
 	const text = `prefix cpmeta: <${config.cpmetaOntoUri}>
-select * where {
+select distinct * where {
 	values ?dobj { ${ids} }
 	?dobj cpmeta:hasObjectSpec ?objSpec ;
 	cpmeta:hasNumberOfRows ?nRows ;
