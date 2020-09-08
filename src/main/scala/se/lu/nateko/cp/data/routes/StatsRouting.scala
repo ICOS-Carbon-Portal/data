4382f8e89f4622ae70ba20270ad9b00d439924d0
Oleg Mirzov <oleg.mirzov@gmail.com>
2020-09-08 18:18:40 +0200

    [stats] Improvements in StatsRouting

--- src/main/scala/se/lu/nateko/cp/data/routes/StatsRouting.scala
+++ src/main/scala/se/lu/nateko/cp/data/routes/StatsRouting.scala
@@ -11,6 +11,8 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import se.lu.nateko.cp.data.CpdataJsonProtocol.javaTimeInstantFormat
 import java.time.Instant
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import scala.concurrent.Future
 
 case class StatsQueryParams(page: Int, pagesize: Int, specs: Option[Seq[String]], stations: Option[Seq[String]], submitters: Option[Seq[String]], contributors: Option[Seq[String]])
 case class DownloadsByCountry(count: Int, countryCode: String)
@@ -33,56 +35,31 @@ class StatsRouting(pgClient: PostgresDlLog, coreConf: MetaCoreConfig) extends De
 	implicit val envriConfs = coreConf.envriConfigs
 	val extractEnvri = UploadRouting.extractEnvriDirective
 
-	val statsParams: Directive1[StatsQueryParams] = parameters(
-		"page".as[Int].?,
-		"pagesize".as[Int].?,
-		"specs".as[List[String]].?,
-		"stations".as[List[String]].?,
-		"submitters".as[List[String]].?,
-		"contributors".as[List[String]].?
-	).tmap{
-		case (page, pagesize, specs, stations, submitters, contributors) =>
-			// TODO: Set default page and pagesize in config? 
-			Tuple1(StatsQueryParams(page.getOrElse(1), pagesize.getOrElse(100), specs, stations, submitters, contributors))
+	def statsQuery[T](lastSegm: String, fetcher: StatsQueryParams => Future[T])(implicit conv: T => ToResponseMarshallable): Route = path(lastSegm){
+		parameters(
+			"page".as[Int].?,
+			"pagesize".as[Int].?,
+			"specs".as[List[String]].?,
+			"stations".as[List[String]].?,
+			"submitters".as[List[String]].?,
+			"contributors".as[List[String]].?
+		){(page, pagesize, specs, stations, submitters, contributors) =>
+			//The constants in the next line can be moved to a config, if needed
+			val pageSize = Math.min(100000, pagesize.getOrElse(100))
+			val qp = StatsQueryParams(page.getOrElse(1), pageSize, specs, stations, submitters, contributors)
+			onSuccess(fetcher(qp)){res =>
+				complete(res)
+			}
+		}
 	}
 
-	val route: Route = (pathPrefix("stats" / "api") & extractEnvri){ implicit envri =>
+	val route: Route = (get & pathPrefix("stats" / "api") & extractEnvri){ implicit envri =>
 
-		path("downloadsByCountry"){
-			statsParams{queryParams =>
-				onSuccess(pgClient.downloadsByCountry(queryParams)){dbc =>
-					complete(dbc)
-				}
-			}
-		} ~
-		path("downloadsPerWeek"){
-			statsParams{queryParams =>
-				onSuccess(pgClient.downloadsPerWeek(queryParams)){dbc =>
-					complete(dbc)
-				}
-			}
-		} ~
-		path("downloadsPerMonth"){
-			statsParams{queryParams =>
-				onSuccess(pgClient.downloadsPerMonth(queryParams)){dbc =>
-					complete(dbc)
-				}
-			}
-		} ~
-		path("downloadsPerYear"){
-			statsParams{queryParams =>
-				onSuccess(pgClient.downloadsPerYear(queryParams)){dbc =>
-					complete(dbc)
-				}
-			}
-		} ~
-		path("downloadStats"){
-			statsParams{queryParams =>
-				onSuccess(pgClient.downloadStats(queryParams)){dbc =>
-					complete(dbc)
-				}
-			}
-		} ~
+		statsQuery("downloadsByCountry", pgClient.downloadsByCountry) ~
+		statsQuery("downloadsPerWeek", pgClient.downloadsPerWeek) ~
+		statsQuery("downloadsPerMonth", pgClient.downloadsPerMonth) ~
+		statsQuery("downloadsPerYear", pgClient.downloadsPerYear) ~
+		statsQuery("downloadStats", pgClient.downloadStats) ~
 		path("specifications"){
 			onSuccess(pgClient.specifications){dbc =>
 				complete(dbc)
