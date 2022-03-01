0b5b26f2057771cf5c9d0a139cf85c4f08c34e99
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2022-03-01 12:13:31 +0100

    [backend] Better merging materialized Future values

--- src/main/scala/se/lu/nateko/cp/data/streams/KeepFuture.scala
+++ src/main/scala/se/lu/nateko/cp/data/streams/KeepFuture.scala
@@ -1,13 +1,28 @@
 package se.lu.nateko.cp.data.streams
 
+import scala.util.{Failure,Success}
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
+import scala.util.Try
+import se.lu.nateko.cp.data.api.CpDataException
 
 object KeepFuture {
 
 	def right[L, R](implicit ctxt: ExecutionContext): (Future[L], Future[R]) => Future[R] =
-		(lf, rf) => for(_ <- lf; r <- rf) yield r
+		merge[L, R, R]((lTry, rTry) => lTry.flatMap(_ => rTry))
 
 	def left[L, R](implicit ctxt: ExecutionContext): (Future[L], Future[R]) => Future[L] =
-		(lf, rf) => for(_ <- rf; l <- lf) yield l
+		merge[L, R, L]((lTry, rTry) => rTry.flatMap(_ => lTry))
+
+	private def merge[L, R, O](tm: (Try[L], Try[R]) => Try[O])(implicit ctxt: ExecutionContext): (Future[L], Future[R]) => Future[O] =
+		(lf, rf) => {
+			lf.transformWith{lTry =>
+				rf.transform{rTry =>
+					(lTry, rTry) match{
+						case (Failure(lErr), Failure(rErr)) => Failure(new CpDataException(lErr.getMessage + "\n" + rErr.getMessage))
+						case _ => tm(lTry, rTry)
+					}
+				}
+			}
+		}
 }
