d509d41cecfaf9fc1b7f824d2ebcfaba130afa65
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2022-08-22 14:38:50 +0200

    [backend] Ignore bad lat/lon when extracting geo-coverage

--- src/main/scala/se/lu/nateko/cp/data/formats/otc/OtcCsvStreams.scala
+++ src/main/scala/se/lu/nateko/cp/data/formats/otc/OtcCsvStreams.scala
@@ -1,18 +1,24 @@
 package se.lu.nateko.cp.data.formats.otc
 
-import java.time._
-import java.time.format.DateTimeFormatter
-
-import akka.stream.scaladsl.{Flow, Keep, Sink}
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.Sink
 import se.lu.nateko.cp.data.api.CpDataParsingException
+import se.lu.nateko.cp.data.formats.TimeSeriesStreams._
 import se.lu.nateko.cp.data.formats._
 import se.lu.nateko.cp.data.formats.csv.CsvParser
-import se.lu.nateko.cp.data.formats.TimeSeriesStreams._
 import se.lu.nateko.cp.data.services.upload.IngestionUploadTask.RowParser
-import se.lu.nateko.cp.data.streams.geo.{GeoFeaturePointSink, Point}
+import se.lu.nateko.cp.data.streams.geo.GeoFeaturePointSink
+import se.lu.nateko.cp.data.streams.geo.Point
 import se.lu.nateko.cp.meta.core.data._
 
-import scala.concurrent.{ExecutionContext, Future}
+import java.time._
+import java.time.format.DateTimeFormatter
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.util.Success
+import scala.util.Try
 
 object OtcCsvStreams {
 
@@ -20,7 +26,7 @@ object OtcCsvStreams {
 	val LatColName = "Latitude"
 	private val csvParser = CsvParser.default
 
-	def socatTsvParser(nRows: Int, format: ColumnsMetaWithTsCol)(implicit ctxt: ExecutionContext): RowParser = Flow[String]
+	def socatTsvParser(nRows: Int, format: ColumnsMetaWithTsCol)(using ExecutionContext): RowParser = Flow[String]
 		.dropWhile(line => !line.contains("*/"))
 		.drop(1)
 		.map(_.trim.split("\t", -1))
@@ -38,7 +44,7 @@ object OtcCsvStreams {
 		.drop(2)
 		.alsoToMat(uploadCompletionSink(format.colsMeta))(Keep.right)
 
-	def otcProductParser(nRows: Int, format: ColumnsMetaWithTsCol)(implicit ctxt: ExecutionContext): RowParser = Flow[String]
+	def otcProductParser(nRows: Int, format: ColumnsMetaWithTsCol)(using ExecutionContext): RowParser = Flow[String]
 		.scan(CsvParser.seed)((acc, line) => csvParser.parseLine(acc, line))
 		.collect{
 			case acc if acc.lastState == CsvParser.Init && acc.cells.length > 0 => acc.cells
@@ -55,13 +61,13 @@ object OtcCsvStreams {
 		.drop(2)
 		.alsoToMat(uploadCompletionSink(format.colsMeta))(Keep.right)
 
-	def uploadCompletionSink(columnsMeta: ColumnsMeta)(implicit ctxt: ExecutionContext): Sink[TableRow, Future[IngestionMetadataExtract]] = {
+	def uploadCompletionSink(columnsMeta: ColumnsMeta)(using ExecutionContext): Sink[TableRow, Future[IngestionMetadataExtract]] = {
 		val tabularExtractSink = digestSink(getCompletionInfo(columnsMeta))
 
 		if(columnsMeta.matchesColumn(LatColName) && columnsMeta.matchesColumn(LonColName))
 			Flow.apply[TableRow]
 				.alsoToMat(tabularExtractSink)(Keep.right)
-				.toMat(coverageSink) { (tsUplComplFut, coverageFut) =>
+				.toMat(coverageSink(ignoreErrors = true)) { (tsUplComplFut, coverageFut) =>
 					for (
 						tsUplCompl <- tsUplComplFut;
 						coverage <- coverageFut
@@ -72,19 +78,12 @@ object OtcCsvStreams {
 			Flow.apply[TableRow].toMat(tabularExtractSink)(Keep.right)
 	}
 
-	def coverageSink(implicit ctxt: ExecutionContext): Sink[TableRow, Future[GeoFeature]] = {
-		Flow.apply[TableRow].map { row =>
+	def coverageSink(ignoreErrors: Boolean)(using ExecutionContext): Sink[TableRow, Future[GeoFeature]] = {
+		val pointsParser: Flow[TableRow, Point, NotUsed] =
+			if(ignoreErrors) Flow.apply[TableRow].mapConcat(parseLatLon(_).toOption)
+			else Flow.apply[TableRow].map(parseLatLon(_).get)
 
-			val lonPos = row.header.columnNames.indexOf(LonColName)
-			val latPos = row.header.columnNames.indexOf(LatColName)
-
-			if (lonPos < 0 || latPos < 0)
-				throw new CpDataParsingException(s"Expected both $LonColName and $LatColName columns to be present")
-
-			val lon = row.cells(lonPos).toDouble
-			val lat = row.cells(latPos).toDouble
-			Point(lon, lat)
-		}.toMat(GeoFeaturePointSink.sink)(Keep.right)
+		pointsParser.toMat(GeoFeaturePointSink.sink)(Keep.right)
 	}
 
 	private def makeSocatTimeStamp(timestamp: String): Instant = {
@@ -92,4 +91,20 @@ object OtcCsvStreams {
 		LocalDateTime.parse(timestamp, timestampFormatter).toInstant(ZoneOffset.UTC)
 	}
 
+	def parseLatLon(row: TableRow): Try[Point] =
+		val lonPos = row.header.columnNames.indexOf(LonColName)
+		val latPos = row.header.columnNames.indexOf(LatColName)
+
+		if (lonPos < 0 || latPos < 0)
+			parseFail(s"Expected both $LonColName and $LatColName columns to be present in row $row")
+		else
+			try
+				val lon = row.cells(lonPos).toDouble
+				val lat = row.cells(latPos).toDouble
+				Success(Point(lon, lat))
+			catch
+				case err: Throwable =>
+					parseFail(s"Problem parsing lat/lon values in row $row\nError message: ${err.getMessage}")
+
+	def parseFail(msg: String) = scala.util.Failure(new CpDataParsingException(msg))
 }
