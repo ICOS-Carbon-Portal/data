b0c64a05d64b19779d625cef779675f555a1f5ff
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2022-10-28 11:25:43 +0200

    [etcfacade] Only reuse relevant fields from ZipEntry

--- src/main/scala/se/lu/nateko/cp/data/streams/ZipEntrySource.scala
+++ src/main/scala/se/lu/nateko/cp/data/streams/ZipEntrySource.scala
@@ -8,14 +8,20 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 import scala.concurrent.ExecutionContext
 
-import ZipEntryFlow.FileEntry
+import ZipEntryFlow.{FileEntry, FileLikeSource}
 
 object ZipEntrySource:
 
-	def fileEntry(zipFile: File, entry: ZipEntry)(using ExecutionContext): FileEntry = entry -> Source
-		.lazySingle(() => ZipFile(zipFile))
-		.flatMapConcat(zip =>
-			StreamConverters
-				.fromInputStream(() => zip.getInputStream(entry))
-				.mapMaterializedValue(_.onComplete(_ => zip.close()))
-		)
+	def fileEntry(zipFile: File, entry: ZipEntry)(using ExecutionContext): FileEntry =
+		val source: FileLikeSource = Source
+			.lazySingle(() => ZipFile(zipFile))
+			.flatMapConcat(zip =>
+				StreamConverters
+					.fromInputStream(() => zip.getInputStream(entry))
+					.mapMaterializedValue(_.onComplete(_ => zip.close()))
+			)
+		val targetEntry = ZipEntry(entry.getName)
+		targetEntry.setTime(entry.getTime)
+		targetEntry.setSize(entry.getSize)
+		targetEntry.setCrc(entry.getCrc)
+		targetEntry -> source
