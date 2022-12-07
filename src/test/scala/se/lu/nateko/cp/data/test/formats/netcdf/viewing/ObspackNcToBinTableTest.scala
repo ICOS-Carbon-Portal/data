8502623aeade98dee17236b8e78a2990b2e22fa6
klarakristina <klarabroman@live.se>
2022-12-07 09:56:30 +0100

    [netcdfts] WIP Refactor tests

--- src/test/scala/se/lu/nateko/cp/data/test/formats/netcdf/viewing/ObspackNcToBinTableTest.scala
+++ src/test/scala/se/lu/nateko/cp/data/test/formats/netcdf/viewing/ObspackNcToBinTableTest.scala
@@ -1,12 +1,9 @@
 package se.lu.nateko.cp.data.test.formats.netcdf.viewing
 
-import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.IOResult
-import akka.stream.scaladsl.FileIO
+import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Source
-import akka.util.ByteString
-import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.funspec.AsyncFunSpec
 import se.lu.nateko.cp.data.formats.ColumnsMeta
 import se.lu.nateko.cp.data.formats.PlainColumn
 import se.lu.nateko.cp.data.formats.TimeSeriesToBinTableConverter
@@ -17,133 +14,101 @@ import se.lu.nateko.cp.data.formats.bintable.BinTableSink
 import se.lu.nateko.cp.data.formats.bintable.FileExtension
 import se.lu.nateko.cp.data.formats.netcdf.ObspackNcToBinTable
 import se.lu.nateko.cp.data.utils.io.withSuffix
-import ucar.nc2.dataset.CoordinateAxisTimeHelper
-import ucar.nc2.dataset.NetcdfDatasets
-import ucar.nc2.time.Calendar
 
 import java.nio.file.Path
-import scala.concurrent.Await
 import scala.concurrent.ExecutionContextExecutor
-import scala.concurrent.duration.DurationInt
-import scala.util.Using
-import java.time.Instant
-import org.scalatest.BeforeAndAfterAll
+import scala.concurrent.Future
 
-class ObspackNcToBinTableTest extends AnyFunSpec with  BeforeAndAfterAll{
+class ObspackNcToBinTableTest extends AsyncFunSpec {
 
 	private given system: ActorSystem = ActorSystem("ObspackNcToBinTableTest")
 	given dispatcher: ExecutionContextExecutor = system.dispatcher
 
-	val path1 = getClass.getResource("/co2_con_aircraft-insitu_42_allvalid_small.nc").getPath
-	val path2 = getClass.getResource("/co2_ssl_tower-insitu_23_allvalid-12magl_small.nc").getPath
+	def readNCFile(path: Path, columnsMeta: ColumnsMeta, debug: String): Future[IndexedSeq[IndexedSeq[String]]] =
+		import ObspackNcToBinTable.TypedVar
 
-	val csvPath = Path.of("src/test/resources/co2_con_aircraft-insitu_42_allvalid_small.csv")
-	val tmpFile = Path.of(path1).withSuffix(FileExtension).withSuffix(".working")
+		val parser = ObspackNcToBinTable(path, columnsMeta).get
+		val tmpFile = path.withSuffix(FileExtension).withSuffix(".working")
 
-	describe("Netcdf reading workbench"){
-		val cm = new ColumnsMeta(Seq(
+		val nRows = parser.schema.size.toInt
+		val readSchema = TimeSeriesToBinTableConverter.getReadingSchema(None, None, nRows, columnsMeta)
+
+		var resFut = Source(parser.readRows()).runWith(BinTableSink(tmpFile.toFile, true))
+			.flatMap{nRowsWritten =>
+				println(s"Written $nRowsWritten")
+
+				val rowsSrc = new BinTableRowReader(tmpFile.toFile, readSchema.binSchema)
+					.rows(readSchema.fetchIndices, 0L, 100)
+					.map{row =>
+						row.indices.map{i =>
+							readSchema.serializers(i)(row(i))
+						}
+					}
+				
+				rowsSrc.runWith(Sink.collection[IndexedSeq[String], IndexedSeq[IndexedSeq[String]]])
+			}.andThen{
+				case _ => parser.close()
+			}
+
+		resFut
+
+	def testDouble(description: String, data: Future[IndexedSeq[IndexedSeq[String]]], i: Int, j: Int, expectedVal: String) =
+		it(description) {
+			data map { columns => assert(columns(i)(j).toDouble == expectedVal.toDouble) }
+		}
+
+	def testString(description: String, data: Future[IndexedSeq[IndexedSeq[String]]], i: Int, j: Int, expectedVal: String) = 
+		it(description) {
+			data map { columns => assert(columns(i)(j) == expectedVal) }
+		}
+
+	val valueVarIndex = 0
+	val timestampVarIndex = 1
+	val latVarIndex = 2
+	val lonVarIndex = 3
+
+	describe("Read values from first file") {
+
+		val columnsMeta = new ColumnsMeta(Seq(
+			PlainColumn(FloatValue, "value", false),
+			PlainColumn(Iso8601DateTime, "time", false),
+			PlainColumn(FloatValue, "latitude", false),
+			PlainColumn(FloatValue, "longitude", false)
+		))
+
+		val path = Path.of(getClass.getResource("/co2_con_aircraft-insitu_42_allvalid_small.nc").getPath)
+		val data = readNCFile(path, columnsMeta, s" path: $path")
+
+		testDouble("First value", data, 0, valueVarIndex, "0.00038232992")
+		testDouble("Second value", data, 1, valueVarIndex, "0.00038208973")
+
+		testString("First timestamp", data, 0, timestampVarIndex, "2005-11-05T02:23:04Z")
+		testString("Second timestamp", data, 1, timestampVarIndex, "2005-11-05T02:23:14Z")
+
+		testString("First latitude value", data, 0, latVarIndex, "35.925")
+		testString("Second latitude value", data, 1, latVarIndex, "35.936")
+
+		testString("First longitude value", data, 0, lonVarIndex, "140.3")
+		testString("Second longitude value", data, 1, lonVarIndex, "140.31")
+
+	}
+
+	describe("Read values from second file") {
+
+		val columnsMeta = new ColumnsMeta(Seq(
 			PlainColumn(FloatValue, "value", false),
 			PlainColumn(Iso8601DateTime, "time", false)
 		))
 
-		def getParserForFile(path: String) =
-			val file = Path.of(path)
-			ObspackNcToBinTable(file, cm).get
+		val path = Path.of(getClass.getResource("/co2_ssl_tower-insitu_23_allvalid-12magl_small.nc").getPath)
+		val data = readNCFile(path, columnsMeta, s" path: $path")
 
-		def countRows(path: String): Int =
-			val watch = new StopWatch
-			import watch.elapsedMs
+		testDouble("First value", data, 0, valueVarIndex, "0.000384392")
+		testDouble("Second value", data, 1, valueVarIndex, "0.000384236")
 
-			println(s"At time $elapsedMs ms, prepared ObspackNcToBinTable for $path")
+		testString("First timestamp", data, 0, timestampVarIndex, "2005-01-01T03:00:00Z")
+		testString("Second timestamp", data, 1, timestampVarIndex, "2005-01-01T04:00:00Z")
 
-			val rows = getParserForFile(path).readRows()
-			println(s"At time $elapsedMs ms, prepared rows Iterable for $path")
-
-			var size: Int = 0
-			rows.foreach{_ => size += 1}
-			println(s"At time $elapsedMs ms, iterated through $size rows")
-			size
-
-
-		ignore("Counts the rows in the files"){
-			countRows(path1)
-			countRows(path2)
-		}
-
-		ignore("Reading date variable"){
-			Using(NetcdfDatasets.openDataset(path1)){ncfile =>
-				val timeVar = ncfile.findVariable("time")
-				val unit = timeVar.attributes().findAttribute("units").getStringValue
-				val timeHelper = new CoordinateAxisTimeHelper(Calendar.gregorian, unit)
-				val ncArr = timeVar.read()
-				val firstDate = timeHelper.makeCalendarDateFromOffset(ncArr.getDouble(0))
-				val lastDate = timeHelper.makeCalendarDateFromOffset(ncArr.getDouble(ncArr.getSize.toInt - 1))
-				println(firstDate)
-				println(lastDate)
-
-				// val sb = new java.lang.StringBuilder();
-				// val formatter = new Formatter(sb, Locale.ENGLISH);
-				// //val group = new Group(ncfile, null, "x07")
-				// val sliceAxis = CoordinateAxis1DTime.factory(ncfile, VariableDS.builder().copyFrom(timeVar).build(null), formatter);
-				// val dList = sliceAxis.getCalendarDates()
-				// println(s"got number of dates: ${dList.size()}")
-				// println(dList.get(0))
-				// println(dList.get(dList.size - 1))
-			}.get
-		}
-
-		it("Parses nc file as CSV"){
-			import ObspackNcToBinTable.TypedVar
-
-			val parser = getParserForFile(path1)
-			val nRows = parser.schema.size.toInt
-			val readSchema = TimeSeriesToBinTableConverter.getReadingSchema(None, None, nRows, cm)
-
-			var resFut = Source(parser.readRows()).runWith(BinTableSink(tmpFile.toFile, true))
-				.flatMap{nRowsWritten =>
-					println(s"Written $nRowsWritten")
-
-					val rowsSrc = new BinTableRowReader(tmpFile.toFile, readSchema.binSchema)
-						.rows(readSchema.fetchIndices, 0L, 100)
-						.map{row =>
-							row.indices.map{i =>
-								readSchema.serializers(i)(row(i))
-							}.mkString("", ",", "\n")
-						}
-						.mapMaterializedValue(_ => NotUsed)
-					val header = Source.single(readSchema.fetchedColumns.mkString("", ",", "\n"))
-					header.concat(rowsSrc).map(t => ByteString(t)).runWith(FileIO.toPath(csvPath))
-				}
-				.andThen{
-					case _ => parser.close()
-				}
-
-			val res = Await.result(resFut, 200.seconds)
-		}
-
-		it("Reads samples from CSV"){
-			val bufferedSource = io.Source.fromFile(csvPath.toFile)
-			val lines = bufferedSource.getLines()
-
-			lines.next() // skip csv header
-
-			val first = lines.next().split(",")
-			val second = lines.next().split(",")
-
-			assert(first(0).toDouble == "0.00038232992".toDouble)
-			assert(second(0).toDouble == "0.00038208973".toDouble)
-
-			assert(first(1).toString == Instant.ofEpochSecond(1131157384).toString)
-			assert(second(1).toString == Instant.ofEpochSecond(1131157394).toString)
-		}
 	}
-
-	override protected def afterAll(): Unit =
-		csvPath.toFile().delete()
-		tmpFile.toFile().delete()
-
-	private class StopWatch:
-		private val start = System.currentTimeMillis()
-		def elapsedMs: Long = System.currentTimeMillis() - start
 
 }
