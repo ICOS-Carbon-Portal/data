0f9a4dbfadc6e95199b3852c27999b45e7e85907
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2023-01-30 16:19:04 +0100

    [backend] Relax third-party distribution test
    Useful for local testing during development

--- src/main/scala/se/lu/nateko/cp/data/utils/io.scala
+++ src/main/scala/se/lu/nateko/cp/data/utils/io.scala
@@ -1,8 +1,25 @@
 package se.lu.nateko.cp.data.utils
 
 import java.nio.file.Path
+import java.net.URI
 
 object io:
 
 	extension(path: Path)
 		def withSuffix(suff: String) = path.resolveSibling(path.getFileName().toString + suff)
+
+	extension(uri: URI)
+		/**
+		  * @return Second-level domain of this URI's host, in case the host is not an IP address. Otherwise, full host.
+		  */
+		def hostL2: String =
+			if uri.hasIpAddressHost then uri.getHost
+			else takeL2Domain(uri.getHost)
+
+		def hasIpAddressHost: Boolean =
+			val chars = uri.getHost.toCharArray
+			(chars.contains('[') && chars.contains(']') || //IPv6
+			chars.forall{c => c == '.' || c.isDigit}) //IPv4
+
+
+	def takeL2Domain(host: String): String = host.split('.').takeRight(2).mkString
