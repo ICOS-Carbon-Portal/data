915b4da0469e92e449a79247868d357a586610cd
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2023-02-02 10:28:58 +0100

    [backend] More robust file writing to disk during upload

--- src/main/scala/se/lu/nateko/cp/data/services/upload/FileSavingUploadTask.scala
+++ src/main/scala/se/lu/nateko/cp/data/services/upload/FileSavingUploadTask.scala
@@ -1,58 +1,62 @@
 package se.lu.nateko.cp.data.services.upload
 
-import java.io.File
-
-import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
-
 import akka.Done
 import akka.stream.scaladsl.FileIO
 import akka.stream.scaladsl.Sink
 import akka.util.ByteString
 import se.lu.nateko.cp.data.api.CpDataException
-import java.nio.file.Paths
+import se.lu.nateko.cp.data.utils.akka.done
+
+import java.io.File
 import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 
-class FileSavingUploadTask(file: File)(implicit ctxt: ExecutionContext) extends UploadTask{
+class FileSavingUploadTask(file: File)(using ExecutionContext) extends UploadTask:
 
-	def sink: Sink[ByteString, Future[UploadTaskResult]] = {
+	private val tmpFile: Path =
+		val path = file.toPath
+		path.resolveSibling(path.getFileName.toString + "_upload")
+
+	def sink: Sink[ByteString, Future[UploadTaskResult]] =
 		if(file.exists)
 			Sink.cancelled.mapMaterializedValue(_ => Future.successful(FileExists))
-		else {
+		else
 			val folder = file.getParentFile
 			if(folder != null && !folder.exists) folder.mkdir()
-			FileIO.toPath(file.toPath).mapMaterializedValue(_.map(ioRes => FileWriteSuccess(ioRes.count)))
-		}
-	}
+			Files.deleteIfExists(tmpFile)
+			FileIO.toPath(tmpFile).mapMaterializedValue(_.map(ioRes => FileWriteSuccess(ioRes.count)))
 
-	def onComplete(ownResult: UploadTaskResult, otherTaskResults: Seq[UploadTaskResult]): Future[UploadTaskResult] = {
-		ownResult match {
-			case FileExists => Future.successful(FileExists)
-			case _ =>
-				val relevantOtherErrors: Seq[UploadTaskFailure] = otherTaskResults.collect{
-					case failure: HashsumCheckFailure => Some(failure)
-					case failure: UnexpectedTaskFailure => Some(failure)
-					case failure: ByteCountingFailure => Some(failure)
+	def onComplete(
+		ownResult: UploadTaskResult,
+		otherTaskResults: Seq[UploadTaskResult]
+	): Future[UploadTaskResult] = ownResult match
 
-					case ByteCountingSuccess(bytes) =>
-						ownResult match{
-							case FileWriteSuccess(actualBytes) =>
-								if(bytes == actualBytes) None else {
-									val msg = s"Got $bytes bytes but written to disk only $actualBytes"
-									Some(ByteCountingFailure(new CpDataException(msg)))
-								}
-							case _ => None
-						}
-				}.flatten
+		case FileExists =>
+			Future.successful(FileExists)
 
-				UploadTask.revertOnAnyFailure(ownResult, relevantOtherErrors, () => Future{
-					if(file.exists) {
-						val newPath = Paths.get(file.getAbsolutePath + "_failed")
-						Files.move(file.toPath, newPath, StandardCopyOption.REPLACE_EXISTING)
-					}
-					Done
-				})
-		}
-	}
-}
+		case _ =>
+			val relevantFailures: Seq[UploadTaskFailure] = otherTaskResults.collect{
+				case failure: HashsumCheckFailure => Some(failure)
+				case failure: UnexpectedTaskFailure => Some(failure)
+				case failure: ByteCountingFailure => Some(failure)
+
+				case ByteCountingSuccess(bytes) =>
+					ownResult match
+						case FileWriteSuccess(actualBytes) =>
+							if bytes == actualBytes then None
+							else
+								val msg = s"Got $bytes bytes but written to disk only $actualBytes"
+								Some(ByteCountingFailure(new CpDataException(msg)))
+						case _ => None
+			}.flatten
+
+			if relevantFailures.isEmpty then ownResult match
+				case _: FileWriteSuccess =>
+					Files.move(tmpFile, file.toPath, StandardCopyOption.ATOMIC_MOVE)
+				case _ => //do not finalize file writing if not full success
+
+			UploadTask.revertOnAnyFailure(ownResult, relevantFailures, () => done)
