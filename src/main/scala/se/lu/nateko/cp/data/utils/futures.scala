2e75d4120e0dd668928a18077fee260e2abf6a46
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2023-07-19 11:54:51 +0200

    [utils] contextualizeFailure extension method on Futures

--- src/main/scala/se/lu/nateko/cp/data/utils/futures.scala
+++ src/main/scala/se/lu/nateko/cp/data/utils/futures.scala
@@ -1,8 +1,10 @@
 package se.lu.nateko.cp.data.utils
 
+import se.lu.nateko.cp.data.api.CpDataException
+
 import java.io.Closeable
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.util.Try
 
 def usingWithFuture[T <: Closeable,O](maker: => T)(user: T => Future[O])(using ExecutionContext): Future[O] = {
@@ -16,3 +18,9 @@ def usingWithFuture[T <: Closeable,O](maker: => T)(user: T => Future[O])(using E
 		}
 	}
 }
+
+extension[T](fut: Future[T])
+	def contextualizeFailure(contextMessage: String)(using ExecutionContext): Future[T] = fut.transform(
+		identity,
+		err => CpDataException(s"$contextMessage: ${err.getMessage}")
+	)
