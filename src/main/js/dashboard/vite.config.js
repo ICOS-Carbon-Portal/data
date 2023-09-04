8b1bc93cc7239506d09c5af1a906a872f7c74c5f
Jonathan Thiry <github@jonathanthiry.com>
2023-09-04 15:02:58 +0200

    [dashboard] Replace gulp with vite

---
+++ src/main/js/dashboard/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/portal',
+    },
+  },
+  server: {
+    hmr: {
+      host: 'localhost'
+    }
+  }
+})
