1ed6b31ba39c58e8021a7333177e9534de57575d
klarakristina <klarabroman@live.se>
2024-04-15 15:08:47 +0200

    [fileformat-validation] fix tests

--- src/test/scala/se/lu/nateko/cp/data/test/streams/NcValidatorTests.scala
+++ src/test/scala/se/lu/nateko/cp/data/test/streams/NcValidatorTests.scala
@@ -1,29 +1,30 @@
 package se.lu.nateko.cp.data.test.streams
 
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.BeforeAndAfterAll
 import akka.actor.ActorSystem
-import se.lu.nateko.cp.data.streams.NetCdfValidator.assertFormat
-import se.lu.nateko.cp.data.streams.NcResult.*
 import akka.stream.scaladsl.FileIO
-import se.lu.nateko.cp.data.test.TestUtils
-import scala.concurrent.Await
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+import se.lu.nateko.cp.data.streams.NetCdfValidator
+import se.lu.nateko.cp.data.streams.NetCdfValidator.*
+import se.lu.nateko.cp.data.test.TestUtils
 
+import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import se.lu.nateko.cp.data.streams.NcResult
 
 
 class NcValidatorTests extends AnyFunSuite with BeforeAndAfterAll:
+	import NetCdfValidator.Result.*
+
 	private given system: ActorSystem = ActorSystem("ZipValidatorTests")
-	val ncSink = assertFormat(NoData, Invalid)(using system.dispatcher)
+	val ncSink = assertFormat(using system.dispatcher)
 
 	override def afterAll(): Unit =
 		system.terminate()
 
 	def testFile(
-		testName: String, inputFile: String, expectedType: NcResult
+		testName: String, inputFile: String, expectedType: Result
 	): Unit = test(testName) {
 		val source = FileIO.fromPath(TestUtils.getFileInTarget(inputFile).toPath)
 		val res = Await.result(source.runWith(ncSink), 2.seconds)
@@ -31,16 +32,16 @@ class NcValidatorTests extends AnyFunSuite with BeforeAndAfterAll:
 		assert(res === expectedType)
 	}
 
-	testFile("Valid nc file", "GCP2019_regions_MASK11_ext.nc", NcResult.Valid)
-	testFile("Valid nc file 2", "GCP2019_regions_MASK11.nc", NcResult.Valid)
-	testFile("Valid HDF file", "amazon_mask.nc", NcResult.Valid)
-	testFile("Invalid nc file", "fluxnet_hh.csv", NcResult.Invalid)
+	testFile("Valid nc file", "GCP2019_regions_MASK11_ext.nc", Valid)
+	testFile("Valid nc file 2", "GCP2019_regions_MASK11.nc", Valid)
+	testFile("Valid HDF file", "amazon_mask.nc", Valid)
+	testFile("Invalid nc file", "fluxnet_hh.csv", Invalid)
 
 	test("No data") {
 		val source = Source(Seq(ByteString.empty))
 		val res = Await.result(source.runWith(ncSink), 2.seconds)
 
-		assert(res === NcResult.NoData)
+		assert(res === NoData)
 	}
 
 
--- src/test/scala/se/lu/nateko/cp/data/test/streams/ZipValidatorTests.scala
+++ src/test/scala/se/lu/nateko/cp/data/test/streams/ZipValidatorTests.scala
@@ -6,23 +6,24 @@ import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
+import se.lu.nateko.cp.data.streams.ZipValidator
 import se.lu.nateko.cp.data.streams.ZipValidator.*
-import se.lu.nateko.cp.data.streams.ZipResult.*
 import se.lu.nateko.cp.data.test.TestUtils
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
-import se.lu.nateko.cp.data.streams.ZipResult
 
 class ZipValidatorTests extends AnyFunSuite with BeforeAndAfterAll:
+	import ZipValidator.Result.*
+
 	private given system: ActorSystem = ActorSystem("ZipValidatorTests")
-	val zipSink = assertFormat(NoData, Invalid)(using system.dispatcher)
+	val zipSink = assertFormat(using system.dispatcher)
 
 	override def afterAll(): Unit =
 		system.terminate()
 
 	def testFile(
-		testName: String, inputFile: String, expectedType: ZipResult
+		testName: String, inputFile: String, expectedType: Result
 	): Unit = test(testName) {
 		val source = FileIO.fromPath(TestUtils.getFileInTarget(inputFile).toPath)
 		val res = Await.result(source.runWith(zipSink), 2.seconds)
@@ -30,16 +31,16 @@ class ZipValidatorTests extends AnyFunSuite with BeforeAndAfterAll:
 		assert(res === expectedType)
 	}
 
-	testFile("Valid zip file", "blablaText.txt.zip", ZipResult.Valid)
-	testFile("Empty zip file", "empty.zip", ZipResult.EmptyZip)
-	testFile("Invalid zip file", "fluxnet_hh.csv", ZipResult.Invalid)
-	testFile("Spanned zip file", "spanned.zip", ZipResult.SpannedZip)
+	testFile("Valid zip file", "blablaText.txt.zip", Valid)
+	testFile("Empty zip file", "empty.zip", EmptyZip)
+	testFile("Invalid zip file", "fluxnet_hh.csv", Invalid)
+	testFile("Spanned zip file", "spanned.zip", SpannedZip)
 
 	test("No data") {
 		val source = Source(Seq(ByteString.empty))
 		val res = Await.result(source.runWith(zipSink), 2.seconds)
 
-		assert(res === ZipResult.NoData)
+		assert(res === NoData)
 	}
 
 	test("Sparse stream"){
@@ -56,5 +57,5 @@ class ZipValidatorTests extends AnyFunSuite with BeforeAndAfterAll:
 		val source = Source(byteStrings)
 		val res = Await.result(source.runWith(zipSink), 2.seconds)
 
-		assert(res === ZipResult.Valid)
+		assert(res === Valid)
 	}
