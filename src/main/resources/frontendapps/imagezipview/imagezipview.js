4b75834fc06de03efbe7a233c64fdbe70c4bcc95
Andrew Debevec <andrew.debevec@nateko.lu.se>
2025-03-06 08:50:53 +0100

    Improve ZIP multi-image viewer styles and add controls (#341)
    Improve imagezipview by styling elements with Bootstrap and adding
    controls to easily change between images, improving the layout and
    making it more functional for users.
    
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1209524177992855

--- src/main/resources/frontendapps/imagezipview/imagezipview.js
+++ src/main/resources/frontendapps/imagezipview/imagezipview.js
@@ -1,29 +1,61 @@
 (function(){
-	const select = document.getElementById('zipimageselect')
-	const image =  document.getElementById('multiimagezipframe')
-	const urlParams = new URLSearchParams(window.location.search)
-	const supportedImages = ['jpg', 'jpeg', 'png']
+	const select = document.getElementById('zipimageselect');
+	const image =  document.getElementById('multiimagezipframe');
+	const urlParams = new URLSearchParams(window.location.search);
+	const supportedImages = ['jpg', 'jpeg', 'png'];
+	const previous = document.getElementById("previous-button");
+	const next = document.getElementById("next-button");
+	let zipEntries = [];
 
-	let zipEntries = []
+	function updateDisplayedImage(step=0) {
+		if(zipEntries.length === 0) return;
 
-	function selectFile(){
-		const idx = select.selectedIndex
-		image.src = idx < 0 || idx >= zipEntries.length ? '' : zipEntries[idx].path
+		// Use local idx variable to prevent auto-wrap to 0 on incrementing over bounds
+		let idx = select.selectedIndex;
+		idx += step;
+
+		if(idx < 0) {
+			idx = 0;
+		} else if (idx >= zipEntries.length) {
+			idx = zipEntries.length-1;
+		}
+
+		select.selectedIndex = idx;
+		
+		image.src = zipEntries[idx].path;
+			
+		previous.disabled = idx <= 0;
+		next.disabled = idx >= zipEntries.length - 1;
 	}
 
-	select.addEventListener('change', selectFile)
+	select.addEventListener('change', _ => updateDisplayedImage());
+	previous.addEventListener('click', _ => updateDisplayedImage(-1));
+	next.addEventListener('click', _ => updateDisplayedImage(1));
+
+	document.addEventListener("keydown", event => {
+		if (event.target.id === "zipimageselect") return;
+		if (event.key === "ArrowLeft") {
+			updateDisplayedImage(-1);
+		} else if (event.key === "ArrowRight") {
+			updateDisplayedImage(1);
+		}
+	});
 
 	fetch(`/zip/${urlParams.get('objId')}/listContents`)
 		.then(resp => resp.json())
 		.then(json => {
 			zipEntries = json.filter(ze => {
-				const fileExt = ze.name.split(".").slice(-1)[0].toLowerCase()
-				return supportedImages.includes(fileExt)
-			})
-			select.innerHTML = zipEntries.map((ze,i) => {
-				const fname = ze.name.split("/").slice(-1)[0]
-				return `<option value=${i}>${fname}</option>`
-			}).join('\n')
+				const fileExt = ze.name.split(".").slice(-1)[0].toLowerCase();
+				return supportedImages.includes(fileExt);
+			});
+
+			zipEntries.forEach((ze,i) => {
+				const fname = ze.name.split("/").slice(-1)[0];
+				const option = document.createElement("option");
+				option.value = i;
+				option.innerText = fname;
+				select.appendChild(option);
+			});
 		})
-		.then(_ => selectFile(), err => console.log(err))
+		.then(_ => updateDisplayedImage(), err => console.log(err));
 })()
