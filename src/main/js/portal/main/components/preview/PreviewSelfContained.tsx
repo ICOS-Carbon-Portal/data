7707d925329b977e517a556aa07a36939ed20360
Andrew Debevec <andrew.debevec@nateko.lu.se>
2025-03-11 15:45:58 +0100

    [preview] Change PreviewSelfContained to function-based component

--- src/main/js/portal/main/components/preview/PreviewSelfContained.tsx
+++ src/main/js/portal/main/components/preview/PreviewSelfContained.tsx
@@ -1,78 +1,65 @@
-import React, {ChangeEvent, Component, CSSProperties} from 'react';
-import config, {PreviewType} from '../../config';
-import {getLastSegmentInUrl} from "../../utils";
-import {State} from "../../models/State";
+import React, { ChangeEvent, useState, useEffect, useRef } from 'react';
+import config, { PreviewType } from '../../config';
+import { getLastSegmentInUrl } from "../../utils";
+import { State } from "../../models/State";
 import { UrlStr } from '../../backend/declarations';
 import { debounce, Events } from 'icos-cp-utils';
 import CartItem from '../../models/CartItem';
 
 
 interface OurProps {
-	preview: State['preview']
-	iframeSrcChange: (event: ChangeEvent<HTMLIFrameElement>) => void
+	preview: State['preview'];
+	iframeSrcChange: (event: ChangeEvent<HTMLIFrameElement>) => void;
 }
 
-type OurState = {
-	height: number | undefined
-}
+export default function PreviewSelfContained({ preview, iframeSrcChange }: OurProps) {
+	const iframeRef = useRef<HTMLIFrameElement>(null);
+	const events = useRef(new Events());
+	const [height, setHeight] = useState<number>(() => getInitialHeight(preview.type));
 
-export default class PreviewSelfContained extends Component<OurProps, OurState>{
-	private ref = React.createRef<HTMLIFrameElement>();
-	private events: any
-	private handleResize: () => void
-
-	constructor(props: OurProps) {
-		super(props);
-
-		this.state = {
-			height: getHeight(props.preview.type)
-		};
-
-		this.events = new Events();
-
-		this.handleResize = debounce(() => {
-			this.ref.current && this.setHeight(this.ref.current)
-		});
-		this.events.addToTarget(window, "resize", this.handleResize);
-	}
-
-	setHeight(iframe: HTMLIFrameElement) {
-		if (shouldUpdateHeight(this.props.preview.type)) {
-			setTimeout(() => {
-				iframe.contentWindow && this.setState({ height: iframe.contentWindow.document.body.scrollHeight + 25 })
-			}, 100)
+	const handleResize = debounce(() => {
+		if (iframeRef.current) {
+			updateHeight(iframeRef.current);
 		}
-	}
+	});
 
-	onLoad(event: ChangeEvent<HTMLIFrameElement>) {
-		this.props.iframeSrcChange(event)
-		this.setHeight(event.target)
-	}
+	useEffect(() => {
+		events.current.addToTarget(window, "resize", handleResize);
+		return () => {
+			events.current.removeFromTarget(window, "resize", handleResize);
+		};
+	}, []);
 
-	shouldComponentUpdate(nextProps: OurProps, nextState: OurState){
-		// Prevent preview component from updating iframe src if we are viewing the same data object
-		return this.props.preview.item.dobj !== nextProps.preview.item.dobj
-			|| this.state.height !== nextState.height;
-	}
+	const updateHeight = (iframe: HTMLIFrameElement) => {
+		if (shouldUpdateHeight(preview.type)) {
+			setTimeout(() => {
+				if (iframe.contentWindow) {
+					setHeight(iframe.contentWindow.document.body.scrollHeight + 25);
+				}
+			}, 300);
+		}
+	};
 
-	render(){
-		const {preview} = this.props;
-		const previewType = preview.type;
+	const handleLoad = (event: ChangeEvent<HTMLIFrameElement>) => {
+		iframeSrcChange(event);
+		updateHeight(event.target);
+	};
 
-		if (previewType === undefined) return null;
+	const previewType = preview.type;
+	if (previewType === undefined) return null;
 
-		const src = getPreviewIframeUrl(previewType, preview.item)
+	const src = getPreviewIframeUrl(previewType, preview.item);
 
-		return (
-			<div className="row" style={{ width: '100%', height: this.state.height }}>
-				<iframe ref={this.ref} onLoad={this.onLoad.bind(this)} src={src} />
-			</div>
-		);
-	}
+	return (
+		<div className="row" style={{ height }}>
+			<iframe ref={iframeRef} onLoad={handleLoad} src={src} />
+		</div>
+	);
+
 }
 
-function getHeight(previewType?: PreviewType): number {
-	switch(previewType){
+function getInitialHeight(previewType?: PreviewType): number {
+	switch (previewType) {
 		case config.NETCDF: return Math.max(window.innerHeight - 100, 480);
 		case config.MAPGRAPH: return 1100;
 		case config.PHENOCAM: return 1100;
@@ -88,11 +75,11 @@ function shouldUpdateHeight(previewType?: PreviewType): boolean {
 	}
 }
 
-function getPreviewIframeUrl(previewType: PreviewType, item: CartItem): UrlStr{
-	const iFrameBaseUrl = config.iFrameBaseUrl[previewType]
+function getPreviewIframeUrl(previewType: PreviewType, item: CartItem): UrlStr {
+	const iFrameBaseUrl = config.iFrameBaseUrl[previewType];
 	// Use preview.item.url if present since that one has all client changes recorded in history
-	if(item.url) return iFrameBaseUrl + getLastSegmentInUrl(item.url)
-	const hashId = getLastSegmentInUrl(item.dobj)
-	if(previewType === config.PHENOCAM) return `${iFrameBaseUrl}?objId=${hashId}`
-	return iFrameBaseUrl + hashId
+	if (item.url) return iFrameBaseUrl + getLastSegmentInUrl(item.url);
+	const hashId = getLastSegmentInUrl(item.dobj);
+	if (previewType === config.PHENOCAM) return `${iFrameBaseUrl}?objId=${hashId}`;
+	return iFrameBaseUrl + hashId;
 }
