faf8cc6f1e2cb3c2a155a25e10ed9aacec2916fe
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-03-31 11:22:37 +0200

    Rewrite comment to be more precise

--- src/main/js/netcdf/main/components/Map.tsx
+++ src/main/js/netcdf/main/components/Map.tsx
@@ -109,7 +109,7 @@ export default function Map(props: MapProps) {
 				const newURL = location.origin + location.pathname + newSearch;
 
 				if (window.frameElement) {
-					// Let calling page (through iframe) know what current url is
+					// Send current URL to parent page (which contains this page in an iframe)
 					window.top!.postMessage(newURL, '*');
 				} else {
 					history.replaceState({urlPath: newURL}, "", newURL);
