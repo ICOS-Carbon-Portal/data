45842d63e2a69fc5909dac8664db9762c06197de
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-04-14 10:01:10 +0200

    Change build tool for portal to Vite (#429)
    Replaace webpack with vite for portal app.

---
+++ src/main/js/portal/vite.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+const buildConf = require('../common/main/buildConf.js');
+
+const appName = path.basename(__dirname);  // 'portal'
+const outDir = path.resolve(buildConf.buildTarget, appName);
+
+module.exports = defineConfig(({ mode }) => ({
+	plugins: [react()],
+	build: {
+		outDir,
+		emptyOutDir: true,
+		sourcemap: true,
+		minify: mode === 'production',
+		rollupOptions: {
+			input: { portal: path.resolve(__dirname, 'main/main.tsx') },
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: '[name]-[hash].js',
+				assetFileNames: (assetInfo) =>
+					assetInfo.name?.endsWith('.css') ? 'css/[name][extname]' : '[name][extname]',
+			}
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				implementation: require('sass'),
+				silenceDeprecations: ['import', 'color-functions', 'global-builtin', 'if-function'],
+			}
+		}
+	}
+}));
