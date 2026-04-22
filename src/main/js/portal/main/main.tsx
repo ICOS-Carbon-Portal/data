0039e927cfa5d931da3ef18f8c5205c7f4716b0b
Jonathan Thiry <github@jonathanthiry.com>
2026-04-22 13:41:24 +0200

    Add Sentry frontend integration

--- src/main/js/portal/main/main.tsx
+++ src/main/js/portal/main/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 import { createRoot } from 'react-dom/client';
+import config from './config';
 import Root from './containers/Root';
 
 import './portal.scss';
@@ -9,6 +11,11 @@ import '../node_modules/react-widgets/scss/multiselect.scss';
 import '../node_modules/ol/ol.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
+if (config.sentry.dsn) {
+	Sentry.init({
+		dsn: config.sentry.dsn,
+	});
+}
 
 const container = document.getElementById('main');
 const root = createRoot(container!);
