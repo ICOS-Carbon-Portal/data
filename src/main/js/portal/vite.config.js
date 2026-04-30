b157b3f2fd93316236a99b5fa80caea335725351
Jonathan Thiry <github@jonathanthiry.com>
2026-04-30 15:47:31 +0200

    Fix Sentry auth token loading

--- src/main/js/portal/vite.config.js
+++ src/main/js/portal/vite.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { defineConfig } = require('vite');
+const { defineConfig, loadEnv } = require('vite');
 const react = require('@vitejs/plugin-react');
 const { sentryVitePlugin } = require('@sentry/vite-plugin');
 const buildConf = require('../common/main/buildConf.js');
@@ -8,9 +8,10 @@ const appName = path.basename(__dirname);  // 'portal'
 const outDir = path.resolve(buildConf.buildTarget, appName);
 
 module.exports = defineConfig(({ mode }) => {
-	const authToken = process.env.SENTRY_AUTH_TOKEN;
+	const env = loadEnv(mode, __dirname, '');
+	const authToken = env.SENTRY_AUTH_TOKEN;
 	if (mode === 'production' && !authToken) {
-		throw new Error('SENTRY_AUTH_TOKEN is required for portal publish builds. Define it in src/main/js/portal/.env.sentry-build-plugin');
+		throw new Error('SENTRY_AUTH_TOKEN is required for portal publish builds. Define it in src/main/js/portal/.env.production.local');
 	}
 
 	return {
